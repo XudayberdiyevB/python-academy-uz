db2cb35e7a385918f015e9524a46abdaa5ab3c7c
Bekzod <xbekzodcoder@gmail.com>
2021-03-06 09:27:15 +0500

    yangi

---
+++ js/index.js
@@ -0,0 +1,13 @@
+var dropx = document.querySelector('.dropx')
+
+var boxX = document.querySelector('.boxX')
+
+dropx.addEventListener('click',function(){
+    if(boxX.classList.contains('active')){
+        boxX.classList.remove('active')
+    }
+    else {
+        boxX.classList.add('active')
+    }
+   
+})
