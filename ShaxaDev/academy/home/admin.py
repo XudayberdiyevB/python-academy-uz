a25764a0e2ddca0df55f6cb4dba6194c8a3101c3
Shaxzod <shaxzadjanaka@gmail.com>
2021-04-08 12:41:16 +0500

    deleted shaxadev papka

--- ShaxaDev/academy/home/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from home.models import CardModel,HomeMaqolaModel,TagModel
-
-# Register your models here.
-admin.site.register(CardModel)
-admin.site.register(HomeMaqolaModel)
-admin.site.register(TagModel)
